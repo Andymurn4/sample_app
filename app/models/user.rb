4ac662a10e0277f3bcf0c2735a75afeb047459b9
Andymurn4 <amurnieks4@gmail.com>
2021-06-23 13:23:21 -0400

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :activation_token
+  attr_accessor :activation_token, :reset_token
   before_save { email.downcase! }
   before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
@@ -39,7 +39,24 @@ class User < ApplicationRecord
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
-end
+
+  #Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  #Sends passwors reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  #Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
 
 private
 
@@ -52,6 +69,7 @@ end
 def create_activation_digest
   self.activation_token = User.new_token
   self.activation_digest = User.digest(activation_token)
+end
 end
 
 
